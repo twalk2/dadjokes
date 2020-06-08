bbe490a49e2950029c545430a43f56574e35f318
twalk2 <2twalk2@gmail.com>
2020-06-08 11:26:52 -0600

    finished joke page and started single jokes component

---
+++ components/Joke.vue
@@ -0,0 +1,22 @@
+<template>
+  <nuxt-link :to="'jokes/' + id">
+    <div class="joke">
+      <p>{{joke}}</p>
+    </div>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  name: "Joke",
+  props: ["joke", "id"]
+};
+</script>
+
+<style  scoped>
+.joke {
+  padding: 1rem;
+  border: 1px dotted gray;
+  margin: 1rem 0;
+}
+</style>
---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>single joke</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>

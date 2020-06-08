0013760012a71eac921666e4475f7e74b6f3652b
twalk2 <2twalk2@gmail.com>
2020-06-08 11:41:21 -0600

    finished single joke component

--- pages/Jokes/_id/index.vue
+++ pages/Jokes/_id/index.vue
@@ -1,9 +1,50 @@
 <template>
-  <div>single joke</div>
+  <div>
+    <nuxt-link to="/jokes">Back</nuxt-link>
+    <h2>{{joke}}</h2>
+    <hr>
+    <small>Joke ID: {{$route.params.id}}</small>
+  </div>
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+export default {
+  data() {
+    return {
+      joke: {}
+    };
+  },
+  async created() {
+    const config = {
+      headers: {
+        Accept: "application/json"
+      }
+    };
+
+    try {
+      const res = await axios.get(
+        `https://icanhazdadjoke.com/j/${this.$route.params.id}`,
+        config
+      );
+      this.joke = res.data.joke;
+    } catch (error) {
+      console.log("error", error);
+    }
+  },
+  head() {
+    return {
+      title: this.joke,
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny Dad Jokes."
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style scoped>

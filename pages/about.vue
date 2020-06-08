28f8558a9da606402bdbbdb9de516f42dd41aa6f
twalk2 <2twalk2@gmail.com>
2020-06-08 10:55:16 -0600

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>About DadJokes</h1>
+    <p>This is an app that displays corny Dad Jokes.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "About The App",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny Dad Jokes."
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h2>Welcome to the best corny dad jokes app on the web.</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Welcome to DadJokes",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Best place for corny Dad Jokes."
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
